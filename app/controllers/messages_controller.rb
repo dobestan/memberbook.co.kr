712f6db46d6ceda7bf7224336fabd31ee0cf75b8
dobestan <dobestan@gmail.com>
2014-11-06 00:57:01 +0900

    리팩토링 : kwargs를 이용해서 문자 발송 로직 추상화

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -2,6 +2,6 @@ class MessagesController < ApplicationController
   include MessagesHelper
 
   def index
-    render text: SMS_API.send_SMS
+    render text: SMS_API.send_SMS(dest_phone: "01022205736", msg_body: "test")
   end
 end
--- app/helpers/messages_helper.rb
+++ app/helpers/messages_helper.rb
@@ -1,6 +1,16 @@
 module MessagesHelper
   class SMS_API
-    def SMS_API.send_SMS()
+    def SMS_API.send_SMS(kwargs)
+      send_time = kwargs[:send_time] if kwargs[:send_time]
+      dest_phone = kwargs[:dest_phone] if kwargs[:dest_phone] # 없을 경우에는 예외처리 필요
+      send_phone = kwargs[:send_phone] ? kwargs[:send_phone] : "01022205736"
+      msg_body = kwargs[:msg_body] if kwargs[:msg_body] # 없을 경우에는 예외처리
+      apiVersion = kwargs[:apiVersion] ? kwargs[:apiVersion] : "1"
+      id = kwargs[:id] ? kwargs[:id] : "dobestan"
+
+      # 이 함수에 대한 구현은 기본적으로 API Store에서 제공하는 소스를 사용하였다.
+      # 다만 조금 더 추상화하여 사용할 수 있도록 한다.
+
       #api request method
       httpmethod = "POST"
 
@@ -9,12 +19,12 @@ module MessagesHelper
 
       #api parameters
       parameters = {}
-      parameters = parameters.merge({"send_time" => ""})
-      parameters = parameters.merge({"dest_phone" => "01022205736"})
-      parameters = parameters.merge({"send_phone" => "01022205736"})
-      parameters = parameters.merge({"msg_body" => "SMS TEST"})
-      parameters = parameters.merge({"apiVersion" => "1"})
-      parameters = parameters.merge({"id" => "dobestan"})
+      parameters = parameters.merge({"send_time" => send_time})
+      parameters = parameters.merge({"dest_phone" => dest_phone})
+      parameters = parameters.merge({"send_phone" => send_phone})
+      parameters = parameters.merge({"msg_body" => msg_body})
+      parameters = parameters.merge({"apiVersion" => apiVersion})
+      parameters = parameters.merge({"id" => id})
 
       #request header (contentType, clientKey)
       contentType = "application/x-www-form-urlencoded"
