669f2709c986eec2123fcf2d45dd8e6419ded756
dobestan <dobestan@gmail.com>
2014-11-06 00:43:57 +0900

    리팩토링 : Helper에서 문자 발송 로직 처리하기

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -2,29 +2,6 @@ class MessagesController < ApplicationController
   include MessagesHelper
 
   def index
-    #api request method
-    httpmethod = "POST"
-
-    #api url
-    url = "http://api.openapi.io/ppurio/1/message/sms/dobestan"
-
-    #api parameters
-    parameters = {}
-    parameters = parameters.merge({"send_time" => ""})
-    parameters = parameters.merge({"dest_phone" => "01022205736"})
-    parameters = parameters.merge({"send_phone" => "01022205736"})
-    parameters = parameters.merge({"msg_body" => "SMS TEST"})
-    parameters = parameters.merge({"apiVersion" => "1"})
-    parameters = parameters.merge({"id" => "dobestan"})
-
-    #request header (contentType, clientKey)
-    contentType = "application/x-www-form-urlencoded"
-    clientKey = "MTkyMC0xNDEzODU0NTAwMzU3LTllM2VkOTM3LTYwMTEtNGU2Zi1iZWQ5LTM3NjAxMTNlNmYyMg=="
-
-    #send request
-    response = MessagesHelper::HttpClient.do_request(httpmethod, url, parameters, clientKey, contentType)
-
-    #api response
-    render text: response
+    render text: SMS_API.send_SMS
   end
 end
--- app/helpers/messages_helper.rb
+++ app/helpers/messages_helper.rb
@@ -1,4 +1,33 @@
 module MessagesHelper
+  class SMS_API
+    def SMS_API.send_SMS()
+      #api request method
+      httpmethod = "POST"
+
+      #api url
+      url = "http://api.openapi.io/ppurio/1/message/sms/dobestan"
+
+      #api parameters
+      parameters = {}
+      parameters = parameters.merge({"send_time" => ""})
+      parameters = parameters.merge({"dest_phone" => "01022205736"})
+      parameters = parameters.merge({"send_phone" => "01022205736"})
+      parameters = parameters.merge({"msg_body" => "SMS TEST"})
+      parameters = parameters.merge({"apiVersion" => "1"})
+      parameters = parameters.merge({"id" => "dobestan"})
+
+      #request header (contentType, clientKey)
+      contentType = "application/x-www-form-urlencoded"
+      clientKey = "MTkyMC0xNDEzODU0NTAwMzU3LTllM2VkOTM3LTYwMTEtNGU2Zi1iZWQ5LTM3NjAxMTNlNmYyMg=="
+
+      #send request
+      response = HttpClient.do_request(httpmethod, url, parameters, clientKey, contentType)
+
+      #api response
+      return response
+    end
+  end
+
   class HttpResponse
     attr :code, true
     attr :raw_body, true
