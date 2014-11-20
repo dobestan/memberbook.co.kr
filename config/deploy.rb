bbf5f8467c0b277434bd2360129ab83faa384d1c
dobestan <dobestan@gmail.com>
2014-11-20 16:35:27 +0900

    deploy.rb 업데이트 : git 주소 추가하기

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,8 +1,8 @@
 # config valid only for Capistrano 3.1
 lock '3.2.1'
 
-set :application, 'my_app_name'
-set :repo_url, 'git@example.com:me/my_repo.git'
+set :application, 'memberbook'
+set :repo_url, 'git@github.com:dobestan/memberbook.co.kr.git'
 
 # Default branch is :master
 # ask :branch, proc { `git rev-parse --abbrev-ref HEAD`.chomp }.call
