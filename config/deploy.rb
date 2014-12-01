453d7d8910c4014c9b49038a0faa2f1a7311b880
dobestan <dobestan@gmail.com>
2014-12-01 22:39:28 +0900

    Migration을 제거하고 테스트 진행

--- config/deploy.rb
+++ config/deploy.rb
@@ -62,7 +62,7 @@ task :deploy => :environment do
     invoke :'git:clone'
     invoke :'deploy:link_shared_paths'
     invoke :'bundle:install'
-    invoke :'rails:db_migrate'
+    # invoke :'rails:db_migrate'
     invoke :'rails:assets_precompile'
     invoke :'deploy:cleanup'
 
