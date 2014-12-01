db113dc80c40e84dc1ecdf7f48680ecc0845f0d1
dobestan <dobestan@gmail.com>
2014-12-01 19:14:27 +0900

    배포 과정 추가 : DB Migration, Assets_Precompile

--- config/deploy.rb
+++ config/deploy.rb
@@ -62,8 +62,8 @@ task :deploy => :environment do
     invoke :'git:clone'
     invoke :'deploy:link_shared_paths'
     invoke :'bundle:install'
-    # invoke :'rails:db_migrate'
-    # invoke :'rails:assets_precompile'
+    invoke :'rails:db_migrate'
+    invoke :'rails:assets_precompile'
     invoke :'deploy:cleanup'
 
     to :launch do
