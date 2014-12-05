063a7c198fa74c0eff40d505e3591181c2f37a1c
Jehyeok <hook3748@gmail.com>
2014-12-05 13:31:07 +0900

    dashboard.js 코드 객체 안으로 전환

--- app/assets/javascripts/dashboard.js
+++ app/assets/javascripts/dashboard.js
@@ -1,138 +1,163 @@
-$('#profileUl').click(function(e) {
-	// li 요소 검색
-	var target = $(e.target).closest('li');
-	$(target).toggleClass('backOn');
-});
+var LeftSidebar = {
+	groupUl: $('#leftSidebar .groupUl'),
+	selectAnotherGroup: function(e) {
+		// li 요소 검색
+		target = $(e.target).closest('li');
 
-$('#navBar #elementUl').click(function(e) {
-	// li 요소 검색
-	var target = $(e.target).closest('li');
-	// li 가 아니라면 리턴
-	if (target[0].tagName !== 'LI') return;
-	// 포커싱 변경
-	var preFocusedElement = $(this).find('.active');
-	preFocusedElement.toggleClass('active');
-	target.toggleClass('active');
+		var groupCode = target.data('code');
+		var groupId = target.data('id');
 
-	// 메인 컨텐츠 변경
-	$('#' + preFocusedElement.data('wrapper')).fadeOut(300, function() {
-		$('#' + target.data('wrapper')).fadeIn(300);
-	});
-});
+		$.ajax({
+			type: 'GET',
+			url: '/dashboard/' + groupCode + '/' + groupId + '/users',
+			success: function(data) {
+				var userTemplate = $('#userTemplate');
+				userTemplate = _.template(userTemplate.text());
 
-// 첫번째 그룹 포커싱 초기화
-$('.group').eq(1).addClass('active'); // hi
+				var profiles = "";
+				var profile;
 
-$('#leftSideBar #thirdRow .groupUl').click(function(e) { // gunyoung park
-	// li 요소 검색
-	target = $(e.target).closest('li');
-	// li 가 아니라면 리턴
-	if (target[0].tagName !== 'LI') return;
+				// 메뉴 포커싱 변경
+				$(this).find('.active').toggleClass('active');
+				target.toggleClass('active');
+				
+				// 각 유저 당 탬플릿 생성
+				$(data).each(function(idx, value) {
+					profile = userTemplate({
+						offsetClass: idx % 2 !== 0 ? 'col-sm-offset-1 col-md-offset-1' : '',
+						name: value.name,
+						grade: value.grade,
+						phone_number: value.phone_number,
+						email: value.email,
+						address: value.address
+					});
 
-	var groupCode = target.data('code');
-	var groupId = target.data('id');
+					profiles += profile;
+				}.bind(this));
 
-	$.ajax({
-		type: 'GET',
-		url: '/dashboard/' + groupCode + '/' + groupId + '/users',
-		success: function(data) {
-			var userTemplate = $('#userTemplate');
-			userTemplate = _.template(userTemplate.text());
+				// 기존 그룹 유저 하이드
+				$('#profileUl .row').fadeOut(300, function() {
+					$(this).children().remove();
+					// 새 그룹 유저 돔에 추가
+					$(profiles).appendTo($(this));
+					$(this).fadeIn(300);
+				});
+			}.bind(this)
+		});
+	},
+	init: function() {
+		// 첫번째 그룹 포커싱 초기화
+		this.groupUl.find('.group').eq(1).addClass('active');
+		this.groupUl.click(this.selectAnotherGroup);
+	}
+};
 
-			var profiles = "";
-			var profile;
+LeftSidebar.init();
 
-			// 메뉴 포커싱 변경
-			$(this).find('.active').toggleClass('active');
+var User = {
+	wrapper: $('#usersWrapper'),
+	profileUl: $('#usersWrapper #profileUl'),
+	toggleUserFace: function(e) {
+		// li 요소 검색
+		var target = $(e.target).closest('li');
+		// 앞면 뒷면 토글
+		$(target).toggleClass('backOn');
+	},
+	init: function() {
+		this.profileUl.click(this.toggleUserFace);
+	}
+};
+
+User.init();
+
+var NavBar = {
+	elementUl: $('#navBar #elementUl'),
+	selectMenu: function(e) {
+		// li 요소 검색
+		var target = $(e.target).closest('li');
+
+		if (target.is('li')) {
+			// 포커싱 변경
+			var preFocusedElement = $(this).find('.active');
+			preFocusedElement.toggleClass('active');
 			target.toggleClass('active');
-			
-			// 각 유저 당 탬플릿 생성
-			$(data).each(function(idx, value) {
-				profile = userTemplate({
-					offsetClass: idx % 2 !== 0 ? 'col-sm-offset-1 col-md-offset-1' : '',
-					name: value.name,
-					grade: value.grade,
-					phone_number: value.phone_number,
-					email: value.email,
-					address: value.address
+
+			// 메인 컨텐츠 변경
+			$('#' + preFocusedElement.data('wrapper')).fadeOut(300, function() {
+				$('#' + target.data('wrapper')).fadeIn(300);
+			});	
+		}
+	},
+	init: function() {
+		this.elementUl.click(this.selectMenu);
+	}
+};
+
+NavBar.init();
+
+var Setting = {
+	wrapper: $('#settingsWrapper'),
+	userTableWrapper: $('.userTableWrapper'),
+
+	activateGroupEdit: function(e) {
+		var ancestor = $(this).parent();
+		var groups = ancestor.find('.group');
+		var target = $(e.target);
+
+		if (target.hasClass('editBtn')) {
+			ancestor.toggleClass('edit');
+		} else if (target.hasClass('saveBtn')) {
+			ancestor.toggleClass('edit');
+		} else {
+			if (ancestor.hasClass('show')) {
+				groups.fadeOut(300);
+				ancestor.toggleClass('show');
+				ancestor.removeClass('edit');
+			} else {
+				groups.fadeIn(300);
+				ancestor.toggleClass('show');
+			}
+		}
+	},
+	addGroup: function(e) {
+		var ancestor = $(this).closest('.ancestor');
+		var groupLevel = ancestor.data('level');
+		var groupCode = ancestor.data('code');
+		var groupId = ancestor.data('id');
+		// 그룹 이름
+		var name = $(this).parent().find('input').val();
+
+		$.ajax({
+			type: 'POST',
+			url: '/dashboard/' + groupCode + '/' + groupId + '/groups',
+			data: {
+				name: name,
+				group_level: groupLevel
+			},
+			success: function(data) {
+				// 그룹 탬플릿 생성
+				var groupTemplate = $('#groupTemplate');
+				groupTemplate = _.template(groupTemplate.text());
+
+				// 그룹 생성
+				var group = groupTemplate({
+					level: data.level,
+					id: data.id,
+					code: data.code,
+					name: data.name
 				});
 
-				profiles += profile;
-			}.bind(this));
-
-			// 기존 그룹 유저 하이드
-			$('#profileUl .row').fadeOut(300, function() {
-				$(this).children().remove();
-				// 새 그룹 유저 돔에 추가
-				$(profiles).appendTo($(this));
-				$(this).fadeIn(300);
-			});
-		}.bind(this)
-	});
-});
-
-$('#settingsWrapper .ancestorContainer').click(function(e) {
-	var ancestor = $(this).parent();
-	var groups = ancestor.find('.group');
-	var target = $(e.target);
-
-	if (target.hasClass('editBtn')) {
-		ancestor.toggleClass('edit');
-	} else if (target.hasClass('saveBtn')) {
-		ancestor.toggleClass('edit');
-	} else {
-		if (ancestor.hasClass('show')) {
-			groups.fadeOut(300);
-			ancestor.toggleClass('show');
-			ancestor.removeClass('edit');
-		} else {
-			groups.fadeIn(300);
-			ancestor.toggleClass('show');
-		}
-	}
-});
-
-$('#settingsWrapper .ancestor .plus').click(function(e) {
-	var ancestor = $(this).closest('.ancestor');
-	var groupLevel = ancestor.data('level');
-	var groupCode = ancestor.data('code');
-	var groupId = ancestor.data('id');
-	// 그룹 이름
-	var name = $(this).parent().find('input').val();
-
-	$.ajax({
-		type: 'POST',
-		url: '/dashboard/' + groupCode + '/' + groupId + '/groups',
-		data: {
-			name: name,
-			group_level: groupLevel
-		},
-		success: function(data) {
-			// 그룹 탬플릿 생성
-			var groupTemplate = $('#groupTemplate');
-			groupTemplate = _.template(groupTemplate.text());
-
-			// 그룹 생성
-			var group = groupTemplate({
-				level: data.level,
-				id: data.id,
-				code: data.code,
-				name: data.name
-			});
-
-			// 돔 추가
-			$(group).insertAfter(ancestor.find('.group').last());
-		},
-		error: function(msg) {
-			alert('그룹추가를 실패했습니다');
-		}
-	});
-});
-
-$('#settingsWrapper .ancestor').click(function(e) {
-	var target = $(e.target);
-	// minus 버튼이 아니라면
-	if (target.hasClass('minus')) {
+				// 돔 추가
+				$(group).insertAfter(ancestor.find('.group').last());
+			},
+			error: function(msg) {
+				alert('그룹추가를 실패했습니다');
+			}
+		});
+	},
+	deleteGroup: function(e) {
+		var target = $(e.target);
+		// minus 버튼이 아니라면
 		var groupId = target.parent().data('id');
 
 		$.ajax({
@@ -145,16 +170,21 @@ $('#settingsWrapper .ancestor').click(function(e) {
 				alert('그룹삭제를 실패했습니다');
 			}
 		});
+	},
+	activateTableUserEdit: function(e) {
+		if ($(e.target).is('.btn')) {
+			$(this).parents('.userTableWrapper').toggleClass('edit');	
+		}
+	},
+	init: function() {
+		this.wrapper.find('.ancestorContainer').click(this.activateGroupEdit);
+		this.wrapper.find('.ancestor .plus').click(this.addGroup);
+		this.wrapper.find('.ancestor .minus').click(this.deleteGroup);
+		this.userTableWrapper.find('.btns').click(this.activateTableUserEdit);
 	}
-});
+};
 
-$('.userTableWrapper .editBtn').click(function(e) {
-	$(this).parents('.userTableWrapper').toggleClass('edit');
-});
-
-$('.userTableWrapper .saveBtn').click(function(e) {
-	$(this).parents('.userTableWrapper').toggleClass('edit');
-})
+Setting.init();
 
 $('.userTableWrapper .userTbodyWrapper').click(function(e) {
 	var target = $(e.target);
