56bf3262dcea12edad8b2bdf7225a438c913a279
Jehyeok <hook3748@gmail.com>
2014-12-18 15:19:41 +0900

    인증 라우팅 및 컨트롤러 변경

--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -1,4 +1,6 @@
 class UserController < ApplicationController
+  include MessagesHelper
+
   # GET /:group_id/users
   def users
     @group = Group.find_by! code: params[:group_code]
@@ -51,4 +53,28 @@ class UserController < ApplicationController
       end
     end
   end 
+
+  def get_confirm_number
+    dest_phone = params[:phone_number]
+
+    authentification_number = 1592
+    response = JSON.parse SMS_API.send_SMS(dest_phone: dest_phone, msg_body: "메세지 테스트입니다.")
+    flash[:authentification_number] = authentification_number
+    session[:hi] = "hi?"
+
+    render plain: 1592
+  end
+
+  def confirm
+    dest_phone = params[:phone_number]
+    authentification_number_input = params[:authentification_number]
+    authentification_number = flash[:authentification_number]
+    puts "input auth num: #{authentification_number_input}"
+    puts "real auth num: #{authentification_number}"
+    puts "session[:hi]: #{session[:hi]}"
+    
+    respond_to do |format|
+      format.json { render plain: "success?"}
+    end
+  end
 end
